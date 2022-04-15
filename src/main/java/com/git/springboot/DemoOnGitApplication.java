991458c76082245ec3cf63717496f5dea0bd7397
Swapna <77243167+swapna2021@users.noreply.github.com>
2022-04-15 11:16:39 +0530

    first commit

---
+++ src/main/java/com/git/springboot/DemoOnGitApplication.java
@@ -0,0 +1,13 @@
+package com.git.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOnGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOnGitApplication.class, args);
+	}
+
+}
