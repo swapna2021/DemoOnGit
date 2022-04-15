b278475d3a89b9509301db28cbc44ff95ebd8e21
yella <yella@LAPTOP-Q9ML3J2S>
2022-04-15 11:39:40 +0530

    committing as ashok

--- src/main/java/com/git/springboot/DemoOnGitApplication.java
+++ src/main/java/com/git/springboot/DemoOnGitApplication.java
@@ -8,6 +8,7 @@ public class DemoOnGitApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoOnGitApplication.class, args);
+		System.out.println(" github");
 	}
 
 }
